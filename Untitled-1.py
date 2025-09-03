14ce6d5ff83cee9e4a96e178e9a6be61d15eabd0
STEFFANI ELIAS GOMES DE OLIVEIRA <steffani.oliveira@aluno.senac.rio>
2025-09-03 18:09:40 -0300

    aula0109

--- Untitled-1.py
+++ Untitled-1.py
@@ -37,13 +37,33 @@ while True:
     except:
         print('Digite um nº valido: ')'''
 
+cadastro = []
+int(input('Informe quantos produtos deseja cadastrar: '))
+            
 print('produto')
 while True:
     try:
-        produto = int(input('Digite o Nº: '))
-        cliente.append(num)
-        sair = input('Deseja adicionar mais um nº? (S/N)').upper().strip()
+        produto = input('Digite o nome o produto: ')
+        cadastro.append(produto)
+        sair = input('Deseja adicionar mais um produto? (S/N)').upper().strip()
         if sair == 'N':
             break
     except:
-        print('Digite um Nº válido.')
+        print('Digite um dado válido.')
+
+print('valor_produto')
+while True:
+    try:
+        valor_produto = float(input('Digite o valor do produto: '))
+        cadastro.append(valor_produto)
+        sair = input('Deseja adicionar mais um valor? (S/N)').upper().strip()
+        if sair == 'N':
+            break
+    except:
+        print('Digite um nº válido.')
+
+for i in cadastro:
+    print(f'')
+
+
+
